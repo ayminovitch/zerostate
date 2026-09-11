ff3b6d3c07e622abe9853211e2e758f3e183a4a7
Aymen Hammami <hello@aymen-hammami.com>
2026-09-11 18:07:57 +0100

    feat(router): add router barrel; wire into root index

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./core/transport/index.js";
 export * from "./core/node/index.js";
 export * from "./core/crdt/index.js";
 export * from "./core/sync/index.js";
+export * from "./core/router/index.js";
 export * from "./errors.js";
