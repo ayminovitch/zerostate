33467ea5368cbd9bbea1b48a805f0a3adbe2051e
Aymen Hammami <hello@aymen-hammami.com>
2026-09-11 18:00:54 +0100

    feat(sync): add sync barrel export; wire into root index

---
+++ src/core/sync/index.ts
@@ -0,0 +1,4 @@
+export { GossipEngine }          from "./gossip.js";
+export type { GossipEvents }     from "./gossip.js";
+export type { GossipCfg, GossipRoundStats } from "./types.js";
+export { DEFAULT_GOSSIP_CFG }    from "./types.js";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./core/transport/index.js";
 export * from "./core/node/index.js";
 export * from "./core/crdt/index.js";
+export * from "./core/sync/index.js";
 export * from "./errors.js";
